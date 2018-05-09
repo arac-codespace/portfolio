cc26c9811f9da0a7bb138f4e7fca369e9ce53508
arac-codespace <alex.aguirre0026@gmail.com>
2018-05-09 15:29:30 -0400

    Changed dev database to PS

--- backend/aracPortfolio/settings.py
+++ backend/aracPortfolio/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'aracPortfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfolio',
+        'USER': os.environ["PG_Username"],
+        'PASSWORD': os.environ["PG_Password"],
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
