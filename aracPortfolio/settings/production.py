354ac6856c0e50fecf005cb2f366e4b92703c782
arac-codespace <alex.aguirre0026@gmail.com>
2018-05-09 22:56:59 -0400

    Split settings file, moved build folder to app base dir.

---
+++ aracPortfolio/settings/production.py
@@ -0,0 +1,16 @@
+from aracPortfolio.settings.common import *
+import dj_database_url
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('PORTFOLIO_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# ALLOWED_HOSTS = ['arac-codespace.herokuapp.com', '127.0.0.1:8000']
+ALLOWED_HOSTS = []
+
+# https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Deployment
+# Heroku: Update database configuration from $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
