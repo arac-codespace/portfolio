195bbf548e79fba71416ded2a60d8193c8965c9f
arac-codespace <alex.aguirre0026@gmail.com>
2018-05-09 23:39:28 -0400

    Used herokus dj-database-url recommended settings

--- aracPortfolio/settings/production.py
+++ aracPortfolio/settings/production.py
@@ -11,5 +11,6 @@ ALLOWED_HOSTS = ['hidden-tundra-44495.herokuapp.com', '127.0.0.1:8000']
 
 # https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Deployment
 # Heroku: Update database configuration from $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
