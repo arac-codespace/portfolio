1116037939aab5c53011aef201a57c8096089098
arac-codespace <alex.aguirre0026@gmail.com>
2018-05-09 23:07:29 -0400

    Tweaked prod settings...

--- aracPortfolio/settings/production.py
+++ aracPortfolio/settings/production.py
@@ -7,8 +7,7 @@ SECRET_KEY = os.environ.get('PORTFOLIO_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# ALLOWED_HOSTS = ['arac-codespace.herokuapp.com', '127.0.0.1:8000']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hidden-tundra-44495.herokuapp.com', '127.0.0.1:8000']
 
 # https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Deployment
 # Heroku: Update database configuration from $DATABASE_URL.
