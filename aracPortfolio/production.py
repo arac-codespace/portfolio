9a39c22416c3020af8b9a3f878129e6459b97d73
arac-codespace <alex.aguirre0026@gmail.com>
2018-05-14 01:59:27 -0400

    Forgot to add allowed hosts...

--- aracPortfolio/production.py
+++ aracPortfolio/production.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['arac-codespace.herokuapp.com', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['alexis-aguirre.herokuapp.com', '127.0.0.1:8000']
 
 # https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Deployment
 # Heroku: Update database configuration from $DATABASE_URL.
